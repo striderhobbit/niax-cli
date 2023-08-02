dbe66d4d308e99732a72a3d244c8d8b3329fd61d
striderhobbit <mariuskoehler@yahoo.de>
2023-08-02 22:32:52 +0200

    pipe/catchError

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UniqItem } from '@shared/schema';
 import { Request } from '@shared/schema/request';
-import { Observable } from 'rxjs';
+import { Observable, OperatorFunction, catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -14,10 +14,11 @@ export class ApiService<T extends UniqItem> {
     params: Request.GetResourceTable<T>['ReqParams'],
     query: Request.GetResourceTable<T>['ReqQuery']
   ): Observable<R> {
-    return this.httpClient.get<R>(
-      `http://localhost:3000/api/resource/table/${params.resource}`,
-      { params: query }
-    );
+    return this.httpClient
+      .get<R>(`http://localhost:3000/api/resource/table/${params.resource}`, {
+        params: query,
+      })
+      .pipe(this.pipeError());
   }
 
   public getResourceTablePage<
@@ -26,19 +27,28 @@ export class ApiService<T extends UniqItem> {
     params: Request.GetResourceTablePage<T>['ReqParams'],
     query: Request.GetResourceTablePage<T>['ReqQuery']
   ): Observable<R> {
-    return this.httpClient.get<R>(
-      `http://localhost:3000/api/resource/table/page/${params.resource}`,
-      { params: query }
-    );
+    return this.httpClient
+      .get<R>(
+        `http://localhost:3000/api/resource/table/page/${params.resource}`,
+        { params: query }
+      )
+      .pipe(this.pipeError());
   }
 
   public patchResourceItem<R extends Request.PatchResourceItem<T>['ResBody']>(
     params: Request.PatchResourceItem<T>['ReqParams'],
     body: Request.PatchResourceItem<T>['ReqBody']
   ): Observable<R> {
-    return this.httpClient.patch<R>(
-      `http://localhost:3000/api/${params.resource}`,
-      body
-    );
+    return this.httpClient
+      .patch<R>(`http://localhost:3000/api/${params.resource}`, body)
+      .pipe(this.pipeError());
+  }
+
+  private pipeError<R>(): OperatorFunction<R, R> {
+    return catchError((error) => {
+      alert(error.message);
+
+      throw error;
+    });
   }
 }
