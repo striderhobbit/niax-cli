8a3ee2e77ac7af79f8b617d90e948e09ee18e9ec
striderhobbit <mariuskoehler@yahoo.de>
2023-08-05 17:43:45 +0200

    ResourceTableComponent route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResourceTableComponent } from './resource-table/resource-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'resource/table',
+    component: ResourceTableComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
