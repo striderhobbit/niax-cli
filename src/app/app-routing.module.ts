2e1fb0ecaa7c310424c7cb294b13fbf031c7b3f7
striderhobbit <mariuskoehler@yahoo.de>
2023-08-14 20:01:02 +0200

    stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,43 +1,37 @@
 import { NgModule, inject } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  RouterModule,
-  RouterStateSnapshot,
-  Routes,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, RouterModule } from '@angular/router';
 import { Resource } from '@shared/schema/resource';
 import { pick } from 'lodash';
 import { Observable } from 'rxjs';
 import { ApiService } from './api.service';
 import { ResourceTableComponent } from './resource-table/resource-table.component';
 
-const routes: Routes = [
-  {
-    component: ResourceTableComponent,
-    path: 'resource/table',
-    resolve: {
-      resourceTable: <I extends Resource.Item>(
-        route: ActivatedRouteSnapshot,
-        state: RouterStateSnapshot
-      ): Observable<Resource.Table<I>> =>
-        inject(ApiService<I>).getResourceTable(
-          pick(
-            route.queryParams,
-            'hash',
-            'limit',
-            'paths',
-            'resourceId',
-            'resourceName'
-          )
-        ),
-    },
-    runGuardsAndResolvers: (from, to) =>
-      from.queryParams['snapshotId'] !== to.queryParams['snapshotId'],
-  },
-];
-
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot([
+      {
+        component: ResourceTableComponent,
+        path: 'resource/table',
+        resolve: {
+          resourceTable: <I extends Resource.Item>(
+            route: ActivatedRouteSnapshot
+          ): Observable<Resource.Table<I>> =>
+            inject(ApiService<I>).getResourceTable(
+              pick(
+                route.queryParams,
+                'hash',
+                'limit',
+                'paths',
+                'resourceId',
+                'resourceName'
+              )
+            ),
+        },
+        runGuardsAndResolvers: (from, to) =>
+          from.queryParams['snapshotId'] !== to.queryParams['snapshotId'],
+      },
+    ]),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
