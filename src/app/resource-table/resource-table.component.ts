64c35a2ec218bc0c442bc6d211ed1638dc8f5db8
striderhobbit <mariuskoehler@yahoo.de>
2023-08-21 11:33:35 +0200

    cacheRestored removed

--- src/app/resource-table/resource-table.component.ts
+++ src/app/resource-table/resource-table.component.ts
@@ -104,7 +104,7 @@ export class ResourceTableComponent<I extends Resource.Item>
            * FIXME https://github.com/angular/components/issues/22022
            */
           if (this.headerRowDef != null) {
-            this.table?.removeHeaderRowDef(this.headerRowDef);
+            // this.table?.removeHeaderRowDef(this.headerRowDef);
           }
 
           this.selection.clear();
